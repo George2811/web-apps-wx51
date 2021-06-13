4b43857cddb4fd88bebbed01e5313916ea3c095a
Sebastian Gonzales Abad <sgabad@hotmail.com>
2021-06-13 15:15:19 -0500

    feature: added terms and conditions view

--- src/app.vue
+++ src/app.vue
@@ -1,30 +1,27 @@
 <template>
   <v-app>
     <tool-bar></tool-bar>
-
     <v-main>
-      <terms-and-conditions-view></terms-and-conditions-view>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
 
-
 import ToolBar from "./components/tool-bar";
-import TermsAndConditionsView from "./components/terms-and-conditions-view";
-
 export default {
   name: 'App',
-
   components: {
-    TermsAndConditionsView,
     ToolBar
-
   },
-
   data: () => ({
     //
   }),
 };
 </script>
+<style>
+main{
+  margin-top: 70px;
+}
+</style>
