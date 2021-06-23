7e3300ca7c8d3f949caca132c254fbe0c6d91c28
George <sanchez28112001@gmail.com>
2021-06-23 00:30:56 -0500

    fix: update deploy

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -2,5 +2,9 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: 'http://sebasgx-001-site1.itempurl.com/api',
-    headers: { 'Content-type':'application/json' }
+    headers: {
+        'Content-type':'application/json',
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+    }
 })
